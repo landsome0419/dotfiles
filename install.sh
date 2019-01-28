7fa2fbd25e709f843d900b222d464bf611ac0c28
landsome0419 <landsome@protonmail.com>
2019-01-28 01:04:09 -0800

    Updated 2 install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,47 @@
-## install firefox
-
+## git config
 echo "Starting Manjaro I3 installation process"
-sleep 3
+echo "
+
+"
+sleep 3 
+echo "configuring git"
+git config --global user.email "landsome@protonmail.com"
+git config --global user.name "landsome0419"
+
+## install firefox
+clear
 echo " Installing Firefox"
+sleep 2
 sudo pacman -S firefox
 
 ## install neofetch
 clear
 echo "Installing Neofetch"
+cd
 git clone https://github.com/dylanaraps/neofetch.git
 
 ## install ruby
+clear
 echo "installing ruby"
-pacman -S ruby
+cd
+sudo pacman -S ruby
 
 ## install lolcat
 clear
 echo "installing lolcat"
-sleep 1
+echo "
+
+"
+sleep 2
 gem install lolcat
 
-## install zsh
-echo "installing ZSH"
-sleep 1
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 ## install zsh fonts
+clear
 echo "installing zsh fonts"
+echo "
+
+"
+sleep 2
 cd
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
@@ -37,10 +52,34 @@ rm -rf fonts
 ## copying dotfiles to computer
 clear
 echo "copying dotfiles to computer"
-cd dotfiles
+echo "
+
+"
+sleep 2
+cd manjaro
 cp config ~/.i3/config 
 cp .Xresources ~/.Xresources
 cp .zshrc ~/.zshrc
+
+
+## install zsh
+echo "installing ZSH"
+sleep 1
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+## status
+echo "Firefox...... Done."
+sleep 1
+echo "Neofetch......Done"
+sleep 1
+echo "ruby..........Done."
+sleep 1
+echo "lolcat........Done."
+sleep 1
+echo "zsh fonts....Done."
+sleep 1
+echo "dotfiles.....Done."
+echo "zsh..........Done."
 echo "Done"
 
 read -p "Would you like to reboot to take effect (y/n) " -n 1 -r
